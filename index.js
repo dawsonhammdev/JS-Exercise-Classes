90f67c353e67670f80d9be12a31b030069654774
dawsonhammdev <hammsdesigns@gmail.com>
2020-02-27 21:33:41 -0600

    Still working on it

--- index.js
+++ index.js
@@ -41,8 +41,26 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age){
+  this.name = name,
+  this.age = age,
+  this.stomach = []
+  }
+  eat(someFood){
+    if (this.stomach.length < 10){
+      return this.stomach.push(someFood);
+  }
 }
+
+  poop(){
+    return (this.stomach = [])
+  }
+
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
+}
+
 
 /*
   TASK 2
@@ -59,6 +77,23 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon){
+  this.model = model,
+  this.milesPerGallon = milesPerGallon,
+  this.tank = 0,
+  this.odometer = 0
+  }
+
+  fill(gallons){
+    return (this.tank += gallons);
+  }
+
+  drive(distance){
+    return 
+  }
+
+  
+
 
 }
 
@@ -75,6 +110,15 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor (given){
+    this.name = given.name,
+    this.age = given.age,
+    this.location = given.location
+  }
+
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 
 }
 
@@ -92,9 +136,12 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor(given){
+    super(given)
+  }
 }
+
 
 /*
   TASK 5
